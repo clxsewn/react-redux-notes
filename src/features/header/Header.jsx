843f33ef00aabc245c4a6b6c94abfeac8785a004
unknown <apelich77@gmail.com>
2022-09-01 23:04:21 +0300

    input loader

--- src/features/header/Header.jsx
+++ src/features/header/Header.jsx
@@ -4,17 +4,24 @@ import { saveNewTodo } from '../todos/todosSlice';
 
 function Header() {
     const [text, setText] = useState('');
+    const [status, setStatus] = useState('idle');
     const dispatch = useDispatch();
 
     const handleChange = (e) => setText(e.target.value);
 
-    const handleKeyDown = (e) => {
+    const handleKeyDown = async (e) => {
         const trimmedText = e.target.value.trim();
         if (e.key === 'Enter' && trimmedText) {
-            dispatch(saveNewTodo(trimmedText));
+            setStatus('loading');
+            await dispatch(saveNewTodo(trimmedText));
             setText('');
+            setStatus('idle');
         }
     };
+
+    let isLoading = status === 'loading';
+    let placeholder = isLoading ? '' : 'What needs to be done?';
+    let loader = isLoading ? <div className='loader' /> : null;
 
     return (
         <header>
@@ -24,7 +31,9 @@ function Header() {
                 value={text}
                 onChange={handleChange}
                 onKeyDown={handleKeyDown}
+                disabled={isLoading}
             />
+            {loader}
         </header>
     );
 }
--- src/store.js
+++ src/store.js
@@ -1,23 +1,14 @@
 import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
 import rootReducer from './reducer';
 import thunkMiddleware from 'redux-thunk';
+import { reduxThunkMiddleware } from './exampleAddons/middleware';
 
-const composed = applyMiddleware(thunkMiddleware);
+// const composed = applyMiddleware(thunkMiddleware);
+const composed = applyMiddleware(reduxThunkMiddleware);
 
 const store = configureStore({
     reducer: rootReducer,
     enhancers: [composed],
 });
-
-export const fetchSomeData = (dispatch, getState) => {
-    fetch('https://jsonplaceholder.typicode.com/todos')
-        .then((response) => response.json())
-        .then((todos) =>
-            dispatch({ type: 'todos/todosLoaded', payload: todos })
-        );
-
-    const allTodos = getState().todos;
-    console.log('Number of todos after loading: ', allTodos.length);
-};
 
 export default store;
